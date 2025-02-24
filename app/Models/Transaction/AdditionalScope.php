4b5b8f68206b6607edfa3f0732445e77d1a60d43
madurabyte <fawait626@gmail.com>
2025-02-24 22:37:40 +0700

    feat: relation assets

--- app/Models/Transaction/AdditionalScope.php
+++ app/Models/Transaction/AdditionalScope.php
@@ -2,12 +2,44 @@
 
 namespace App\Models\Transaction;
 
+use App\Enums\ScopeStandartTypeEnum;
 use App\Traits\SettingModel;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class AdditionalScope extends Model
 {
     use SettingModel;
 
     protected $connection = 'transaction';
+
+    public function assetWelnes(): HasOne
+    {
+        return $this->hasOne(ScopeStandartAsset::class, 'scope_standart_uuid')->where('category', ScopeStandartTypeEnum::ASSET_WELNESS->value)->latest();
+    }
+
+    public function ohRecom(): HasOne
+    {
+        return $this->hasOne(ScopeStandartAsset::class, 'scope_standart_uuid')->where('category', ScopeStandartTypeEnum::OH_RECOM->value)->latest();
+    }
+
+    public function woPriority(): HasOne
+    {
+        return $this->hasOne(ScopeStandartAsset::class, 'scope_standart_uuid')->where('category', ScopeStandartTypeEnum::WO_PRIORITY->value)->latest();
+    }
+
+    public function history(): HasOne
+    {
+        return $this->hasOne(ScopeStandartAsset::class, 'scope_standart_uuid')->where('category', ScopeStandartTypeEnum::HISTORY->value)->latest();
+    }
+
+    public function rla(): HasOne
+    {
+        return $this->hasOne(ScopeStandartAsset::class, 'scope_standart_uuid')->where('category', ScopeStandartTypeEnum::RLA->value)->latest();
+    }
+
+    public function ncr(): HasOne
+    {
+        return $this->hasOne(ScopeStandartAsset::class, 'scope_standart_uuid')->where('category', ScopeStandartTypeEnum::NCR->value)->latest();
+    }
 }
