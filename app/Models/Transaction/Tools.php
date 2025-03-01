423d83a21d25356f4fcdf6304b6dbf1a19fd0e50
Achmad Fawait <fawait626@gmail.com>
2025-03-01 23:04:34 +0700

    feat: relation global unit

--- app/Models/Transaction/Tools.php
+++ app/Models/Transaction/Tools.php
@@ -2,12 +2,19 @@
 
 namespace App\Models\Transaction;
 
+use App\Models\GlobalUnit;
 use App\Traits\SettingModel;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Tools extends Model
 {
     use SettingModel;
 
     protected $connection = 'transaction';
+
+    public function globalUnit(): BelongsTo
+    {
+        return $this->belongsTo(GlobalUnit::class, 'global_unit_uuid');
+    }
 }
