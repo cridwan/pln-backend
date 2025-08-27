3aeef225c728b30222b880d50f331e264b72cb73
Achmad Fawait <fawait626@gmail.com>
2025-08-27 18:41:16 +0700

    fix: transaction

--- app/Http/Requests/Transaction/CloneEquipmentRequest.php
+++ app/Http/Requests/Transaction/CloneEquipmentRequest.php
@@ -24,8 +24,8 @@ class CloneEquipmentRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'equipment_uuid' => ['required', Rule::exists(ConnectionEnum::GLOBAL->value . '.equipment', 'uuid')],
-            'scope_standart_uuid' => ['required', Rule::exists(ConnectionEnum::TRANSACTION->value . '.scope_standarts', 'uuid')]
+            'equipment_uuid' => ['required', Rule::exists(ConnectionEnum::GLOBAL ->value . '.equipment', 'uuid')],
+            'scope_standart_uuid' => ['required', Rule::exists(ConnectionEnum::GLOBAL ->value . '.scope_standarts', 'uuid')]
         ];
     }
 }
