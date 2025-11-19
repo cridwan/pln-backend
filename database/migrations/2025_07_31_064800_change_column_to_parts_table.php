e33a995d33692a282d985f422f20de2e88cf85cc
Achmad Fawait <fawait626@gmail.com>
2025-11-19 16:07:43 +0700

    fix: migration

--- database/migrations/2025_07_31_064800_change_column_to_parts_table.php
+++ database/migrations/2025_07_31_064800_change_column_to_parts_table.php
@@ -5,8 +5,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -16,8 +15,6 @@ return new class extends Migration
             $table->dropForeign(['activity_uuid']);
             $table->dropColumn('activity_uuid');
             $table->dropColumn('qty');
-            $table->dropColumn('size');
-            $table->dropColumn('location');
             $table->decimal('price', 20, 2)->default(0);
             $table->string('merk')->nullable();
         });
