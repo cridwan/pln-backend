78e911f3f0b0d57445487a756f960f09cab3aa94
Achmad Fawait <fawait626@gmail.com>
2025-12-16 20:58:28 +0700

    fix: trigger

--- database/migrations/transactions/2025_12_16_133845_create_trigger.php
+++ database/migrations/transactions/2025_12_16_133845_create_trigger.php
@@ -12,30 +12,23 @@ return new class extends Migration {
     public function up(): void
     {
         DB::unprepared("
-        DELIMITER $$
             CREATE TRIGGER DEL_ACTIVITY BEFORE DELETE ON activities FOR EACH ROW BEGIN
                 DELETE from cons_mat_stds WHERE cons_mat_stds.activity_uuid = OLD.uuid;
                 DELETE FROM part_stds WHERE part_stds.activity_uuid = OLD.uuid;
                 DELETE FROM manpower_stds WHERE manpower_stds.activity_uuid = OLD.uuid;
             END
-            $$
-        DELIMITER ;");
+        ");
         DB::unprepared("
-        DELIMITER $$
             CREATE TRIGGER DEL_ADD_SCOPE BEFORE DELETE ON additional_scopes FOR EACH ROW BEGIN
                 DELETE FROM scope_standarts where scope_standarts.additional_scope_uuid = OLD.uuid;
             END
-            $$
-        DELIMITER ;");
+        ");
         DB::unprepared("
-        DELIMITER $$
             CREATE TRIGGER DEL_EQUIPMENT BEFORE DELETE ON equipment FOR EACH ROW BEGIN
                 DELETE FROM activities WHERE activities.equipment_uuid = OLD.uuid;
             END
-            $$
-        DELIMITER ;");
+        ");
         DB::unprepared("
-        DELIMITER $$
             CREATE TRIGGER DEL_PROJECT BEFORE DELETE ON projects FOR EACH ROW BEGIN
                 DELETE FROM scope_standarts where scope_standarts.project_uuid = OLD.uuid;
                 DELETE FROM additional_scopes WHERE additional_scopes.project_uuid = OLD.uuid;
@@ -43,15 +36,12 @@ return new class extends Migration {
                 DELETE FROM qc_plans WHERE qc_plans.project_uuid = OLD.uuid;
                 delete from project_activities where project_activities.project_uuid = OLD.uuid;
             END
-            $$
-        DELIMITER ;");
+        ");
         DB::unprepared("
-        DELIMITER $$
             CREATE TRIGGER DEL_SCOPE_STANDAR BEFORE DELETE ON scope_standarts FOR EACH ROW BEGIN
                 DELETE FROM equipment WHERE equipment.scope_standart_uuid = OLD.uuid;
             END
-            $$
-        DELIMITER ;");
+        ");
     }
 
     /**
